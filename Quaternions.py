2030ccaaffb3e6dfc536d9791d52e48f42540dfc
itsmenelll <nadellaneil2@gmail.com>
2025-12-31 23:41:09 +0530

    Initial commit of quaternion rotation simulation code

--- Quaternions.py
+++ Quaternions.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 
-# ----------------- Quaternion Utilities -----------------
+#Quaternion properties and operations
 
 def normalize(q):
     return q / np.linalg.norm(q)
@@ -25,11 +25,11 @@ def quat_conjugate(q):
 
 def rotate_vector(q, v):
     """Rotate 3D vector v using quaternion q"""
-    qv = np.array([0, *v])
-    return quat_mult(quat_mult(q, qv), quat_conjugate(q))[1:]
+    qv = np.array([0, *v]) #converts 3D vector to vector quaternion
+    return quat_mult(quat_mult(q, qv), quat_conjugate(q))[1:] #rotaion operation
 
 
-# ----------------- Cube Geometry -----------------
+#Cube Geometry
 
 cube = np.array([
     [-1, -1, -1],
@@ -59,17 +59,17 @@ ax.set_box_aspect([1,1,1])
 
 lines = [ax.plot([], [], [], 'k')[0] for _ in edges]
 
-q = np.array([1,0,0,0])  # identity quaternion
+q = np.array([1,0,0,0])  # identity quaternion 
 
-axis = normalize(np.array([1, 1, 0.3]))
+axis = normalize(np.array([1, 1, 0.3])) # rotation axis or u in q = sin(theta/2) + u*cos(theta/2)
 theta = 0.3
-dq = normalize(np.array([np.cos(theta/2), *(axis*np.sin(theta/2))]))
+dq = normalize(np.array([np.cos(theta/2), *(axis*np.sin(theta/2))])) #dq for incremental rotation
 
 def update(frame):
     global q
-    q = normalize(quat_mult(dq, q))
+    q = normalize(quat_mult(dq, q))#take the existing cube rotationthen rotate it a little more
 
-    rotated = np.array([rotate_vector(q, v) for v in cube])
+    rotated = np.array([rotate_vector(q, v) for v in cube]) #finally using the quaternion operation to rotate each vertex
 
     for (i,(a,b)) in enumerate(edges):
         xs = [rotated[a,0], rotated[b,0]]
